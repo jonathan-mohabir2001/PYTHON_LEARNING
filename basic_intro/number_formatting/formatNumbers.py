d87d07da7bd6c57652256167877f48084229ba71
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-09-12 17:32:40 -0400

    PYTHON

---
+++ basic_intro/number_formatting/formatNumbers.py
@@ -0,0 +1,15 @@
+# This python file will be practise of formatting numbers. 
+x = 3.0 
+b = -1234
+y = 17 
+z = 34.12344
+
+print(abs(b)); 
+# the abs format above turns the negative b variable into a postive number. 
+print(float(y)); 
+# the float format above turns the integer number into a decimal number. 
+print(round(z,3)); 
+#the round format above rounds the decimal number to 2 decimal places. Second parameter 
+# passed when using the round method will specifcy the number of decimal places. 
+print(str(y)); 
+
---
+++ basic_intro/script.py
@@ -0,0 +1,13 @@
+#This will be my first lines of Python. It is a language I will 
+#be learning within my third semester. 
+# I am also engaging in my own textbook to help with fundamentals. 
+
+x = -4; 
+y = abs(x); 
+print (x); 
+print(y); 
+# The lines above are creating a simple variable, assigning it a negative 
+# and then the absoulte math method will convert the negative to positive. 
+
+#Refer to python 3  built in functions for more insight.  end of first lines of python. 
+
